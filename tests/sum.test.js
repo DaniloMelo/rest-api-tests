2dbda48a61166cfb4216e439a280491d272307f7
Danilo Melo <danilomarquesdemelo@gmail.com>
2024-05-02 15:26:08 -0300

    feat: install and configure jest to use ecmascript modules

---
+++ tests/sum.test.js
@@ -0,0 +1,11 @@
+import sum from "../src/sum.js"
+
+describe("Test sum.js", () => {
+  it("Shold return 4 when sum 2 + 2", () => {
+    expect(sum(2, 2)).toBe(4)
+  })
+
+  it("Shold return 6 when sum 4 + 2", () => {
+    expect(sum(4, 2)).toBe(6)
+  })
+})
