72c91bd79a624303aa0e0e90603ae2db6b19fc43
Danilo Melo <danilomarquesdemelo@gmail.com>
2024-05-04 16:36:08 -0300

    feat: create post test and route

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from "express"
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get("/", (req, res) => {
   res.status(200).send(`<h1>Home</h1>`)
 })
@@ -12,6 +15,11 @@ app.get("/users", (req, res) => {
   ]
 
   res.status(200).json(user)
+})
+
+
+app.post("/users", (req, res) => {
+  res.status(201).json(req.body);
 })
 
 export default app
--- tests/user.test.js
+++ tests/user.test.js
@@ -14,8 +14,19 @@ describe("Test /users route and responses", () => {
     expect(response.body).toHaveLength(1)
   })
 
-  it("Shold return name and value on body", async () => {
+  it("Should return name and value on body", async () => {
     const response = await request.get("/users")
     expect(response.body[0]).toHaveProperty("name", "John Doe")
+  })
+
+  it("Should create a new user and return 201 HTTP Status Code", async () => {
+    const response = await request.post("/users").send({ name: "Jane Doe", email: "jane@email.com" })
+    expect(response.status).toBe(201)
+  })
+
+  it("Shold return name property when create a new user on /users", async () => {
+    const response = await request.post("/users").send({ name: "Jane Doe", email: "jane@email.com" })
+    console.log(response.body)
+    expect(response.body).toHaveProperty("name", "Jane Doe")
   })
 })
